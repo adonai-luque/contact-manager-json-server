8b462986a6e3bc70d4644e5bae47d016c5fa0814
adonai-luque <akluqueprieto@gmail.com>
2021-07-19 03:25:28 -0500

    Trying to make it run in Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = Number(process.env.PORT || 30001;
+const port = Number(process.env.PORT || 3001);
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {
